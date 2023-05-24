647b7c88f2f2f6fe6df5447173bc8a5a7687f25d
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2023-05-24 18:29:54 +0200

    navbar done

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="wrapper">
+        <div className="logo">anima</div>
+        <nav className="menu-item1">
+          <ul>
+            <li>
+              <a href="#">Početna</a>
+            </li>
+            <li>
+              <a href="#">O nama</a>
+            </li>
+            <li>
+              <a href="#">Kontakt</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="menu-item2">
+        <button>
+          <a href="#">Prijavi se</a>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
