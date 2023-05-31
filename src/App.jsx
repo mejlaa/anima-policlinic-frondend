65bf6c848ac298a26ac553517371d47442803ae8
mirza salkovic <salkovic.mirza@gmail.com>
2023-05-31 18:20:42 +0200

    add App folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import "./App.css";
-import Register from "./Components/Register/Register";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Admin from "./Pages/Admin/Admin";
+import Footer from "./Components/Footer/Footer";
+import Usluge from "./Pages/Usluge/Usluge";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <Register></Register>
+    <div id="appMain">
+      <BrowserRouter>
+        <Routes>
+          <Route path="aboutUs" element={<AboutUs />} />
+          <Route path="/" element={<Usluge />} />
+          <Route path="admin" element={<Admin />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
